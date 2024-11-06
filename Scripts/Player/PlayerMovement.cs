d83183f05e8db6cfac94b75ea61d788f4c916985
tran1174 <ahutran@ucdavis.edu>
2024-11-05 21:46:10 -0800

    fixed playerMovement

--- Scripts/Player/PlayerMovement.cs
+++ Scripts/Player/PlayerMovement.cs
@@ -1,7 +1,7 @@
 using System;
 using Godot;
 
-public partial class player : CharacterBody2D
+public partial class PlayerMovement : CharacterBody2D
 {
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
