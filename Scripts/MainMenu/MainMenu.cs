2d60eb5822923a11f1574e6aeb580045abd03ed8
tran1174 <ahutran@ucdavis.edu>
2024-11-06 16:59:12 -0800

    added basic buttons
    play and quit functionality
    building settings later

---
+++ Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class MainMenu : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	// public override void _Ready()
+	// {
+	// }
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	// public override void _Process(double delta)
+	// {
+	// }
+	public void OnPlayPressed()
+	{
+		GetTree().ChangeSceneToFile("res://Scenes/MainScene/main_scene.tscn");
+	}
+	public void OnSettingsPressed()
+	{
+		//Todo
+	}
+	public void OnQuitPressed()
+	{
+		GetTree().Quit();
+	}
+}
