99bcd89f523fbf39d43fdb5fcf81c9176d3295ec
LocT02 <xxthegreeksxx@gmail.com>
2024-11-12 16:38:12 -0800

    fixed gravity

--- Scripts/Player/PlayerMovement.cs
+++ Scripts/Player/PlayerMovement.cs
@@ -5,16 +5,17 @@ using Godot;
 public partial class PlayerMovement : CharacterBody2D
 {
 	public float Speed {get; set;} = 400.0f;
+	public float Gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
 	public override void _PhysicsProcess(double delta)
 	{
 
 		Vector2 velocity = Velocity;
-		
+
 		// Add the gravity.
 		if (!IsOnFloor())
 		{
-			Velocity += GetGravity() * (float)delta;
+			velocity.Y += Gravity * (float)delta;
 		}
 
 		velocity.X = 0;
