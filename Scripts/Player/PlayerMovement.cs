8bfd6f0bb39a34a29d8177b2322d96dff5f2dbfa
LocT02 <xxthegreeksxx@gmail.com>
2024-11-12 16:32:49 -0800

    fixed movement quirks

--- Scripts/Player/PlayerMovement.cs
+++ Scripts/Player/PlayerMovement.cs
@@ -5,21 +5,27 @@ using Godot;
 public partial class PlayerMovement : CharacterBody2D
 {
 	public float Speed {get; set;} = 400.0f;
-	
-	public void GetInput(){
-		Vector2 _inputDirection = Input.GetVector("Move_Left", "Move_Right", "ui_up", "ui_down");
-		Velocity = _inputDirection * Speed;
-	}
 
 	public override void _PhysicsProcess(double delta)
 	{
+
+		Vector2 velocity = Velocity;
+		
 		// Add the gravity.
 		if (!IsOnFloor())
 		{
 			Velocity += GetGravity() * (float)delta;
 		}
 
-		GetInput();
+		velocity.X = 0;
+		if (Input.IsActionPressed("Move_Left")){
+			velocity.X = -Speed;
+		}
+		else if (Input.IsActionPressed("Move_Right")){
+			velocity.X = Speed;
+		}
+
+		Velocity = velocity;
 		MoveAndSlide();
 	}
 }
