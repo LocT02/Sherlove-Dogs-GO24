95781206f04f19abfc830120d75344cb823834cf
LocT02 <xxthegreeksxx@gmail.com>
2024-11-12 16:24:29 -0800

    Movement moved to A,D
    right now up and down arrow key makes the player go up and down

--- Scripts/Player/PlayerMovement.cs
+++ Scripts/Player/PlayerMovement.cs
@@ -1,40 +1,25 @@
 using System;
+using System.ComponentModel;
 using Godot;
 
 public partial class PlayerMovement : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	public float Speed {get; set;} = 400.0f;
+	
+	public void GetInput(){
+		Vector2 _inputDirection = Input.GetVector("Move_Left", "Move_Right", "ui_up", "ui_down");
+		Velocity = _inputDirection * Speed;
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
-
 		// Add the gravity.
 		if (!IsOnFloor())
 		{
-			velocity += GetGravity() * (float)delta;
+			Velocity += GetGravity() * (float)delta;
 		}
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		{
-			velocity.Y = JumpVelocity;
-		}
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
-
-		Velocity = velocity;
+		GetInput();
 		MoveAndSlide();
 	}
 }
